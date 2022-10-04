fa7451067741a122d1fecbbfc9ccb16a12f002f2
Mohammad Alfayoume <mabed3819@gmail.com>
2022-10-05 01:23:21 +0300

    fibonacci done by iteration method

---
+++ math_series/math_script.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if type(n) != int:
+        return "You have to insert integer number"
+    if n == 0 or n == 1:
+        return n
+    num1 = 0
+    num2 = 1
+    sum = 0
+    for x in range(2, n+1):
+        sum = num1 + num2
+        num1 = num2
+        num2 = sum
+    return sum
