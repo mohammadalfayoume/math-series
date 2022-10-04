cc76de5e818b63843276b0f109767ccfe5b7494c
Mohammad Alfayoume <mabed3819@gmail.com>
2022-10-05 01:47:18 +0300

    lucas test done by iteration

--- math_series/math_script.py
+++ math_series/math_script.py
@@ -11,3 +11,19 @@ def fibonacci(n):
         num1 = num2
         num2 = sum
     return sum
+
+def lucas(n):
+    if type(n) != int:
+        return "You have to insert integer number"
+    if n == 0:
+        return 2
+    if n == 1:
+        return n
+    num1 = 2
+    num2 = 1
+    sum = 0
+    for x in range(2, n+1):
+        sum = num1 + num2
+        num1 = num2
+        num2 = sum
+    return sum
